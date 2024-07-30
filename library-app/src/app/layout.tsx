544795a937cfe9f6fc43441cec6c2ff50939ce3b
KELVIN KANYITI GBOLO <kelvingbolo98@gmail.com>
2024-07-30 20:20:26 +0000

    Add: menu list

--- library-app/src/app/layout.tsx
+++ library-app/src/app/layout.tsx
@@ -1,20 +1,37 @@
+"use client"
 import type { Metadata } from "next";
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import {useEffect,useState, useRef } from "react";
 
 
 
-export const metadata: Metadata = {
+const metadata: Metadata = {
   title: "Library App",
   description: "An app which targets users below the ages of 18",
 };
 
-export default function RootLayout({children,}:{children:React.ReactNode}) {
+export default function RootLayout({ children, }: { children: React.ReactNode }) {
+  const browse_links = ["subjects", "trending", "lists", "My Books"]
+  const contribs = ["Add a Book"]
+  const [showmenu, setShowMenu] = useState(false);
+  const menuRef = useRef(null);
+  const renderMenu = () => {
+      setShowMenu(!showmenu)
+  }
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (menuRef.current && !menuRef.current.contains(event.target as Node)) {
+        setShowMenu(false);
+      }
+    };
+    document.addEventListener('mousedown', handleClickOutside);
+  }, [])
   return (
     <html>
       <body className="bg-creamy-100">
-        <header className="flex flex-row justify-between p-2 text-lg">
+        <header className="flex flex-row justify-between p-2 text-lg md:pr-[9em]">
           <div className="p-2 font-extrabold text-2xl flex space-x-6">
             {/* This contains the Logo and the Name of the application */}
             <p>Let&lsquo;s Learn</p>
@@ -46,9 +63,37 @@ export default function RootLayout({children,}:{children:React.ReactNode}) {
             </div>
             <a href="" className="hidden sm:block w-[5em] hover:text-green-500 transition-colors p-1 pt-2 font-bold font-mono">Log in</a>
             <button className="hidden sm:block bg-green-500 hover:bg-creamy-100 hover:text-green-500 hover:border-green-500 transition-all w-[5em] h-[2em] rounded-md font-mono border-solid border-2 border-black mt-1">JOIN</button>
-            <div className="p-2">
-              <Image src="/images/menu.png" alt="menu button" className="cursor-pointer" width={30} height={50} />
-            </div>
+            <button className="p-2" onClick={renderMenu}>
+              <Image src="/images/menu.png" alt="menu button" width={30} height={50} />
+            </button>
+            { 
+              showmenu && (
+                <>
+                  <div className="fixed inset-0 bg-black opacity-25 z-10" onClick={() => setShowMenu(false)}></div>
+                  <section ref={menuRef} className="absolute bg-orange-200 shadow-2xl z-[1000] -right-[0.5px] md:right-[1em] w-[11em] rounded-t-md h-[20em]">
+                    <p className="text-center border-b-2 border-b-yellow-800 font-description">my Let&apos;s learn</p>
+                    <div className="flex space-x-2 md:space-x-3 py-2">
+                      <button className="bg-creamy-100 hover:bg-creamy-100 hover:text-blue-500 hover:border-blue-500 transition-all w-[5em] h-[2em] rounded-md font-mono border-solid border-2 border-black mt-1">log in</button>
+                      <button className="bg-green-500 hover:bg-creamy-100 hover:text-green-500 hover:border-green-500 transition-all w-[5em] h-[2em] rounded-md font-mono border-solid border-2 border-black mt-1">JOIN</button>
+                    </div>
+                    <p className="text-center border-b-2 border-b-yellow-800 font-description mt-3">Browse</p>
+                    <ul>
+                      {
+                        browse_links.map((link, link_index) => (
+                          <li key={link_index} className="pt-2 hover:bg-creamy-100 hover:bg-opacity-20"><a href="" className="pl-2">{link}</a> </li>
+                        ))
+                      }
+                    </ul>
+                    <p className="text-center border-b-2 border-b-yellow-800 font-description mt-3">Contribute</p>
+                    {
+                      contribs.map((contrib, contrib_index) => (
+                        <p key={contrib_index} className="pl-2 shadow-md shadow-green-400 w-fit bg-creamy-100 ml-1aa"><a href="">{ contrib}</a></p>
+                      ))
+                    }
+                    </section>
+                  </>
+              )
+            }
           </div>
         </header>
         <main>{children}</main>
