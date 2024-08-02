77f3fc086a24af5fa0e4b6776eebe80d752c90c6
KELVIN KANYITI GBOLO <kelvingbolo98@gmail.com>
2024-08-02 15:33:41 +0000

    Refactor: codes to achieve the best results

--- library-app/src/app/page.tsx
+++ library-app/src/app/page.tsx
@@ -1,7 +1,9 @@
 "use client";
 import Image from "next/image";
 import Link from "next/link";
-import { useEffect,useState } from "react";
+import { useEffect, useState } from "react";
+import GenreCompo from "./components/GenreComp";
+import { Genre } from "./components/types";
 
 export default function Home() {
   const getGenreFromDjangoAPI = async () => {
@@ -9,67 +11,22 @@ export default function Home() {
     const data = await response.json();
     return data
   }
-
-  const getBooksByGenre = async (genreId:  number) => {
-    const response = await fetch(`http://127.0.0.1:8000/api/books/books/genre/${genreId}`);
-    const data = await response.json();
-    return data;
-  };
-
-  const books = [
-    { name: "To Kill a Mockingbird", author: "Harper Lee", dateOfPublication: "1960-07-11", genre: "kids" },
-    { name: "1984", author: "George Orwell", dateOfPublication: "1949-06-08", genre: "kids" },
-    { name: "The Great Gatsby", author: "F. Scott Fitzgerald", dateOfPublication: "1925-04-10", genre: "kids" },
-    { name: "The Catcher in the Rye", author: "J.D. Salinger", dateOfPublication: "1951-07-16", genre: "kids" },
-    { name: "life", author: "CoDed", dateOfPublication: "2020-05-01", genre: "kids" },
-    { name: "Things fall apart", author: "Author 6", dateOfPublication: "2020-06-01", genre: "kids" },
-    { name: "Dessert Rivers", author: "Author 7", dateOfPublication: "2020-07-01", genre: "kids" },
-  ];
-  const kidsBooks = books.filter(book => book.genre === "kids")
-  const [currentPage, setCurrentPage] = useState(0);
-  const [booksPerPage, setBooksPerPage] = useState(2);
-  const [genres, setGenres] = useState([]);
-  const [booksByGenre, setBooksByGenre] = useState({});
+  const [genres, setGenres] = useState<Genre[]>([]);
   const [loading, setLoading] = useState(true);
-  
+
   useEffect(() => {
-    const updateBooksPerPage = () => {
-        setBooksPerPage(window.innerWidth < 640 ? 2 : 5);
-    };
     const fetchGenresAndBooks = async () => {
       try {
         const genresData = await getGenreFromDjangoAPI();
         setGenres(genresData);
-        const booksData = {};
-        for (const genre of genresData) {
-          booksData[genre.id] = await getBooksByGenre(genre.id);
-        }
-        setBooksByGenre(booksData);
       } catch (error) {
-        console.error('Error fetching genres or books:', error);
+        console.error('Error fetching genres', error);
       } finally {
         setLoading(false);
       }
     };
-
-    
-
-    updateBooksPerPage(); // Set initial value
-    window.addEventListener('resize', updateBooksPerPage); // Update on resize
     fetchGenresAndBooks();
-    return () => window.removeEventListener('resize', updateBooksPerPage); // Cleanup on unmount
   }, []);
-
-  const totalPages = Math.ceil(kidsBooks.length / booksPerPage);
-
-  const handleNext = () => {
-    setCurrentPage((prevPage) => (prevPage + 1) % totalPages);};
-
-  const handlePrev = () => {
-      setCurrentPage((prevPage) => (prevPage - 1 + totalPages) % totalPages);
-  };
-  const startIndex = currentPage * booksPerPage;
-  const selectedBooks = kidsBooks.slice(startIndex, startIndex + booksPerPage);
   
   if (loading) {
     return <div>Loading...</div>;
@@ -112,29 +69,10 @@ export default function Home() {
           <button><Image src="/images/forward.png" alt="forward icon" className="pt-9 w-[2.5em] md:w-[3em]" width={70} height={30}/></button>
         </div>
       </section>
+      {/* Genre Component Goes in here */}
       {
         genres.map((genre) => (
-          <section key={genre.id} className="mt-[3em]">
-            <p className="p-2 font-title text-2xl">{genre.genre}</p>
-            <div className="flex space-x-4 justify-center">
-              <button onClick={handlePrev}><Image src="/images/backward.png" alt="back icon" className="pt-9 w-[2.5em] md:w-[3em] absolute right-[20.99em] md:right-[80em] top-[28.5em]" width={70} height={30} /></button>
-              <div className="flex space-x-2 md:space-x-10">
-                {
-                  booksByGenre[genre.id]?.map(((book, b_index) => (
-                    <Link key={b_index} href="/bookdetail" className="bg-yellow-300 w-[8em] md:w-[10em] shadow-2xl flex flex-col justify-end md:block max-h-[16em] md:max-h-none">
-                      <p className="h-[13em] max-h-[13em] md:w-[10em] bg-blue-400">
-                        <Image src={book.cover_image} alt={book.title} width={40} height={40} />
-                      </p>
-                      <p className="font-projects pl-2">{ book.title}</p>
-                      <p className="font-title pl-2">{ book.publisher}</p>
-                      <p className="flex justify-end pr-1">{ book.publication_date}</p>
-                    </Link>
-                    )))
-                }
-              </div>
-              <button onClick={handleNext}><Image src="/images/forward.png" alt="forward icon" className="pt-9 w-[2.5em] md:w-[3em] absolute left-[20.99em] md:left-[80em] top-[28.5em]" width={70} height={30}/></button>
-            </div>
-          </section>
+          <GenreCompo key={genre.id} genre={genre}/>
         ))
       }
     </main>
