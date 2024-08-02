d3fb3d6f526ee17e54845d20818d3b2273fae8e4
KELVIN KANYITI GBOLO <kelvingbolo98@gmail.com>
2024-08-02 15:22:55 +0000

    Convert: Genre and Book into their own components

---
+++ library-app/src/app/components/BookComp.tsx
@@ -0,0 +1,26 @@
+// components/BookComp.tsx
+import React from 'react';
+import Image from 'next/image';
+import { Book as BookType } from './types';
+import Link from 'next/link';
+
+interface BookProps {
+  book: BookType;
+}
+
+const BookComp: React.FC<BookProps> = ({ book }) => {
+  return (
+    <Link href="/bookdetail" className="bg-orange-200 bg-opacity-25 w-[8em] md:w-[10em] shadow-2xl flex flex-col max-h-[19em] md:max-h-none">
+        <p className="h-[13em] max-h-[13em] md:w-[10em]">
+            <Image src={book.cover_img} alt={book.title} className="w-full h-full" width={90} height={30} />
+        </p>
+        <div className="mt-auto">
+            <p className="font-projects pl-2">{ book.title}</p>
+            <p className="font-title pl-2">{ book.publisher}</p>
+            <p className="flex justify-end pr-1">{ book.publication_date}</p>
+        </div>
+    </Link>
+  );
+};
+
+export default BookComp;
---
+++ library-app/src/app/components/GenreComp.tsx
@@ -0,0 +1,73 @@
+// components/GenreCompo.tsx
+import React from 'react';
+import { Genre, Book } from './types';
+import { useEffect, useState } from 'react';
+import BookComp from './BookComp';
+import Image from 'next/image';
+
+interface GenreProps {
+  genre: Genre;
+}
+
+const GenreCompo: React.FC<GenreProps> = ({ genre }) => {
+    const [books, setBooks] = useState<Book[]>([]);
+    const [currentPage, setCurrentPage] = useState(0);
+    const [booksPerPage, setBooksPerPage] = useState(2);
+
+    useEffect(() => {
+        const updateBooksPerPage = () => {
+            setBooksPerPage(window.innerWidth < 640 ? 2 : 5);
+        };
+
+        const fetchBooks = async () => {
+          const response = await fetch(`http://127.0.0.1:8000/api/books/books/genre/${genre.id}`);
+          const data = await response.json();
+          setBooks(data);
+        };
+        updateBooksPerPage(); // Set initial value
+        window.addEventListener('resize', updateBooksPerPage);
+
+        fetchBooks();
+        return () => window.removeEventListener('resize', updateBooksPerPage);
+    }, [genre.id]);
+    const totalPages = Math.ceil(books.length / booksPerPage);
+    const handleNext = () => {
+        setCurrentPage((prevPage) => (prevPage + 1) % totalPages);};
+    
+    const handlePrev = () => {
+        setCurrentPage((prevPage) => (prevPage - 1 + totalPages) % totalPages);
+      };
+    const startIndex = currentPage * booksPerPage;
+    const selectedBooks = books.slice(startIndex, startIndex + booksPerPage);
+    
+    return (
+        <section key={genre.id} className="mt-[3em]">
+            <p className="p-2 font-title text-2xl">{genre.genre}</p>
+            <div className="flex justify-center relative">
+            { 
+                totalPages > 1 && (
+                    <button onClick={handlePrev}>
+                        <Image src="/images/backward.png" alt="back icon" className="pt-9 w-[2.5em] md:w-[3em] relative -left-2 md:-left-[70px]" width={70} height={30} />
+                    </button>
+                )
+            } 
+            <div className="flex space-x-2 md:space-x-10">
+                {
+                    selectedBooks.map((book) => (
+                        <BookComp key={book.id} book={book} />
+                    ))
+                }
+            </div>
+            {
+                totalPages > 1 && (
+                    <button onClick={handleNext}>
+                        <Image src="/images/forward.png" alt="forward icon" className="pt-9 w-[2.5em] md:w-[3em] relative -right-2 md:-right-[70px]" width={70} height={30} />
+                    </button>    
+                ) 
+            }
+        </div>
+    </section>
+  );
+};
+
+export default GenreCompo;
