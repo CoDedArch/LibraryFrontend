1839b8d5f4625ee5a6a6f06f918fd7bef4b7a836
KELVIN KANYITI GBOLO <kelvingbolo98@gmail.com>
2024-08-03 21:46:21 +0000

    minor modification

--- library-app/src/app/bookdetail/[id]/page.tsx
+++ library-app/src/app/bookdetail/[id]/page.tsx
@@ -119,7 +119,7 @@ export default function BookDetails({ params }: { params: {id:string} }) {
                         <ul className="flex space-x-3">
                             <li className="absolute top-[13em] md:static flex"><Image src="/images/star2.png" alt="star" width={30} height={30} /><span className="pl-2  mt-1 font-bold">{book.average_rating}</span></li>
                             <li className="border-l-2 border-l-black mt-1 pl-2"> {book.number_of_ratings} ratings</li>
-                            <li className="border-l-2 border-l-black mt-1 pl-2"> 180 want to read</li>
+                            <li className="border-l-2 border-l-black mt-1 pl-2"> { 123 } want to read</li>
                             <li className="border-l-2 border-l-black mt-1 pl-2"> {book.readers_currently_reading} currently reading</li>
                             <li className="border-l-2 border-l-black mt-1 pl-2"> {book.readers_finished_reading} have read</li>
                         </ul>
