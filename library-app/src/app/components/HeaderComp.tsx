5ca6a99d1bf9f6f5fbd43c0839479407991ef1d4
KELVIN KANYITI GBOLO <kelvingbolo98@gmail.com>
2024-08-03 21:15:02 +0000

    Enhance functionality of UI

--- library-app/src/app/components/HeaderComp.tsx
+++ library-app/src/app/components/HeaderComp.tsx
@@ -11,14 +11,22 @@ const HeaderComp: React.FC = () => {
     const contribs = ["Add a Book"]
     const browse_links = ["all books", "trending", "lists", "My Books"]
     const [showmenu, setShowMenu] = useState(false);
+    const [showBrowseMenu, setShowBrowseMenu] = useState(false);
+    const [showSearchBar, setShowSearchBar] = useState(false);
     const [isLoggedIn, setIsLoggedIn] = useState(false);
     const menuRef = useRef<HTMLDivElement>(null);
 
     const router = useRouter();
     const renderMenu = () => {
-        setShowMenu(!showmenu)
-
+        setShowMenu(!showmenu);
     }
+
+    const browseMenu = () => {
+        setShowBrowseMenu(!showBrowseMenu);
+    }
+    const handleSearchIconClick = () => {
+        setShowSearchBar(window.innerWidth < 640 ? !showSearchBar : false);
+    };
     useEffect(() => {
         const checkUserIsAuthenticated = async () => {
             const response = await fetch(AUTHENTICATED);
@@ -58,34 +66,48 @@ const HeaderComp: React.FC = () => {
 
     return (
         <header className="flex flex-row justify-between p-2 text-lg md:pr-[9em]">
+            <ul className={`${showBrowseMenu ? 'block':'hidden'} absolute top-[6.4em] right-3 md:top-[3.4em] md:left-[32em] z-[10000] bg-orange-200 md:bg-opacity-80 rounded-md w-[10em]`}>
+                {
+                    browse_links.map((link, link_index) => (
+                        <li key={link_index} className="my-3 pt-2 hover:bg-creamy-100 border-b-2 hover:bg-opacity-60"><Link href={link === "all books" ? "/" : `/${link}`} className="pl-2">{link}</Link></li>
+                    ))
+                }
+            </ul>
             <div className="p-2 font-extrabold text-2xl flex space-x-6">
             {/* This contains the Logo and the Name of the application */}
-            <Link href="/">Let&lsquo;s Learn</Link>
+            <Link href="/" className={`${showSearchBar ? 'hidden' : 'block'}`}>Let&lsquo;s Learn</Link>
             <p className="absolute top-[2.4em] md:static font-extralight text-2xl md:text-lg p-1 md:bg-orange-200 bg-opacity-25 hover:bg-opacity-10 transition-opacity">
                 <a href="http://" className=" hover:text-orange-300 transition-colors">My Books</a>
             </p>
             </div>
             <div className="flex md:space-x-2">
-                <button className="flex flex-row relative top-[3em] left-[4em] md:static md:p-2">
-                <p className="font-extralight font-mono w-fit text-2xl">Browse</p> 
-                <div className="p-3 pl-[2px] w-[2em]">
-                    <Image src="/images/dropdown.png" alt="drop down" width={30} height={20} className="w-[0.7em]"/>
-                </div>
+                <button onClick={browseMenu} className={`flex flex-row absolute ${isLoggedIn ? 'top-[4.3em] right-[2em]' : 'top-[3.4em] right-[2em]'} md:static md:p-2`}>
+                    <p className="font-extralight font-mono w-fit text-2xl">Browse</p> 
+                    <div className="p-3 pl-[2px] w-[2em]">
+                        { 
+                            showBrowseMenu ? (
+                                    <Image src="/images/up.png" alt="drop down" width={30} height={20} className="w-[0.7em]"/>
+                            ) : (
+                                    
+                                    <Image src="/images/dropdown.png" alt="drop down" width={30} height={20} className="w-[0.7em]"/>
+                            )
+                        }
+                    </div>
                 </button>
             {/* this is the search bar */}
-                <div className="hidden w-[15em] md:w-[20em] md:flex flex-row justify-between rounded-md border-r-2 border-orange-200">
-                <form action="">
-                <select name="" id="" className="h-[2.8em] bg-orange-200 opacity-55 border-r-2 border-black rounded-l-md">
-                    <option value="">All</option>
-                </select>
-                <input type="text" placeholder="Search" className="pl-1 h-[2.6em] w-[9em] md:w-[14.3em] bg-orange-200 bg-opacity-20 text-black font-normal ml-1 border-r-2 border-black placeholder:text-black placeholder:font-light outline-none" />
-                </form>
-                <div className="p-2">
-                <Image src="/images/barcode.png" alt="barcode reader" className="cursor-pointer" width={30} height={50}/>
+                <div className={`${showSearchBar ? 'block' : 'hidden'} w-[15em] md:w-[20em] md:flex flex-row justify-between rounded-md border-r-2 border-orange-200`}>
+                    <form action="">
+                    <select name="" id="" className="h-[2.8em] bg-orange-200 opacity-55 border-r-2 border-black rounded-l-md">
+                        <option value="">All</option>
+                    </select>
+                    <input type="text" placeholder="Search" className="pl-1 h-[2.6em] w-[9em] md:w-[14.3em] bg-orange-200 bg-opacity-20 text-black font-normal ml-1 border-r-2 border-black placeholder:text-black placeholder:font-light outline-none" />
+                    </form>
+                    <div className="md:p-2 md:static absolute top-5 right-[5.5em]">
+                    <Image src="/images/barcode.png" alt="barcode reader" className="cursor-pointer" width={30} height={50}/>
+                    </div>
                 </div>
-            </div>
-            <div className="relative md:-left-28 p-3">
-                <Image src="/images/search.png" alt="search icon" className="w-[1.4em] cursor-pointer" width={40} height={50}/>
+                <div className="relative md:-left-28 p-3">
+                    <Image src="/images/search.png" alt="search icon" onClick={handleSearchIconClick} className="w-[1.4em] cursor-pointer" width={40} height={50}/>
                 </div>
                 { 
                     isLoggedIn ? (
@@ -94,8 +116,16 @@ const HeaderComp: React.FC = () => {
                         <Link href="/login" className="hidden sm:block w-[5em] hover:text-green-500 transition-colors p-1 pt-2 font-bold font-mono">Log in</Link>           
                     )
                 }
-            <Link href="/signup" className="hidden sm:block text-center pt-1 bg-green-500 hover:bg-creamy-100 hover:text-green-500 hover:border-green-500 transition-all w-[5em] h-[2em] rounded-md font-mono border-solid border-2 border-black mt-1">JOIN</Link>
-            <button className="p-2" onClick={renderMenu}>
+                { 
+                    isLoggedIn ? (
+                        <div className="pt-2 font-extralight underline">
+                            <Image src="/images/acct.png" className="w-[2em] cursor-pointer" title="View Account" alt="profile" width={70} height={30} />
+                        </div>           
+                    ) : (
+                            <Link href="/signup" className={`${showSearchBar ? 'hidden' : 'block' } text-center pt-1 bg-green-500 hover:bg-creamy-100 hover:text-green-500 hover:border-green-500 transition-all w-[5em] h-[2em] rounded-md font-mono border-solid border-2 border-black mt-1`}>JOIN</Link>                    
+                    )  
+                }    
+            <button className={`p-2 ${isLoggedIn ? 'absolute md:static':'static'} top-[4em] right-1`} onClick={renderMenu} title="Pop Up Menu">
                 <Image src="/images/menu.png" alt="menu button" width={30} height={50} />
             </button>
             { 
@@ -107,12 +137,19 @@ const HeaderComp: React.FC = () => {
                             <div className="flex space-x-2 md:space-x-3 py-2">
                             {
                                 isLoggedIn ? (
-                                    <button onClick={handleLogout} className="hidden sm:block right-[18em] text-center hover:bg-red-400 hover:text-white hover:border-black transition-all w-[5em] h-[2em] rounded-md font-mono border-solid border-2 border-red-400 mt-1">Log out</button>
+                                    <button onClick={handleLogout} className="text-center hover:bg-red-400 hover:text-white hover:border-black transition-all w-[5em] h-[2em] rounded-md font-mono border-solid border-2 border-red-400 mt-1">Log out</button>
                                 ) : (
                                     <Link href="/login" className="text-center pt-1 bg-creamy-100 hover:bg-creamy-100 hover:text-blue-500 hover:border-blue-500 transition-all w-[5em] h-[2em] rounded-md font-mono border-solid border-2 border-black mt-1">log in</Link>          
                                 )         
+                            }
+                            { 
+                                isLoggedIn ? (
+                                    <div className="pt-2 font-extralight underline">View Profile</div>           
+                                ) : (
+                                    <Link href="/signup" className="text-center pt-1 bg-green-500 hover:bg-creamy-100 hover:text-green-500 hover:border-green-500 transition-all w-[5em] h-[2em] rounded-md font-mono border-solid border-2 border-black mt-1">JOIN</Link>                    
+                                )  
                             }        
-                            <Link href="/signup" className="text-center pt-1 bg-green-500 hover:bg-creamy-100 hover:text-green-500 hover:border-green-500 transition-all w-[5em] h-[2em] rounded-md font-mono border-solid border-2 border-black mt-1">JOIN</Link>
+                            
                         </div>
                         <p className="text-center border-b-2 border-b-yellow-800 font-description mt-3">Browse</p>
                         <ul>
