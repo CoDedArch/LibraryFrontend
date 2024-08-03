35c49632fa56b67bb8436805883b54a69e29b074
KELVIN KANYITI GBOLO <kelvingbolo98@gmail.com>
2024-08-03 13:37:49 +0000

    serverside logic to set a user token

--- library-app/src/app/api/login/route.jsx
+++ library-app/src/app/api/login/route.jsx
@@ -1,13 +1,10 @@
-import { cookies } from 'next/headers'
 import { NextResponse } from 'next/server'
+import {setToken,setRefreshToken} from '@/app/lib/auth'
 
 
 const Ninja_API_LOGIN_URL = "http://127.0.0.1:8000/api/token/pair"
 export async function POST(request) {
-    const myAuthToken = cookies().get('auth-token')
-    console.log(myAuthToken.value)
-    
-    const requestData = await request.json()
+    const requestData = await request.json();
     const jsonData = JSON.stringify(requestData);
     const requestOptions = {
         method: "POST",
@@ -21,28 +18,14 @@ export async function POST(request) {
         const response = await fetch(Ninja_API_LOGIN_URL, requestOptions);
         const responseData = await response.json();
         if (response.ok) {
-            console.log("Logged in successfully");
-            const authToken =  responseData.access
-            cookies().set(
-                {
-                    name: 'auth-token',
-                    value: authToken,
-                    httpOnly: true,
-                    sameSite: 'strict',
-                    secure: process.env.NODE_ENV !== 'development',
-                    maxAge:3600,
-                }
-            ) 
-            // Handle successful login (e.g., save token, redirect user)
+            const {access, refresh} =  responseData
+            setToken(access)
+            setRefreshToken(refresh)
+            return NextResponse.json({ loggedIn: true}, { status: 200 });
         } else {
-            console.error("Login failed:");
-            // Handle login failure (e.g., show error message)
+            return NextResponse.json({ loggedIn: false, ...responseData },{ status: 400 });
         }
     } catch (error) {
-        console.error("Error during login:", error);
-        // Handle network or other errors
+        return NextResponse.json({ message: "An error occurred during login", error: error.message }, { status: 500 });
     }
-    // const authToken = cookies().get("auth-token")
-    
-    return NextResponse.json({ "hello": "world"}, {status:200})
 }
